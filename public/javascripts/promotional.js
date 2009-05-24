de42398bbb3def48b7086d74839ebeb4857a5ece
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-05-24 15:40:42 +0100

    tidied up the sign-up process

--- public/javascripts/promotional.js
+++ public/javascripts/promotional.js
@@ -5,3 +5,39 @@ $(document).ready(function(){
 		return false;
 	})
 })
+
+// live validation on the sign-up form
+// still need some work on the live validation part!
+// var forms = new Array(".new_user input");
+	
+$(document).ready(function(){
+	$(forms.join(", ")).keyup(function(){
+		current_element = $(this);
+		var form = $(this).parents().filter('form');
+		$(this).delay(800, function(){
+			var location = form.attr("action").toString()
+			location = location + "/validate"
+			$.get(location, form.serialize(), display_validation_errors, "json")
+			return false
+		})
+	})
+})
+
+function display_validation_errors(data){
+	// remove existing errors
+	$("div.fieldWithErrors").unwrap().remove();
+	$("span.errorMessage").remove();
+	
+	// apply errors
+	jQuery.each(data.errors, function(index, value){
+		var name = value[0];
+		var message = value[1];
+		var field = $("#" + data.model + "_" + name);
+		var label = $("label[for=" + data.model + "_" + name +"]");
+		if(!field.parent().hasClass("fieldWithErrors")){
+			label.after("<span class='errorMessage'>"+ name + " " + message + " </span>");
+			field.wrap("<div class='fieldWithErrors'></div>");
+		}
+	});
+	window.current_element.focus();
+}
