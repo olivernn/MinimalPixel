07db6e6365eb9b38a82ab8856e6776ef0f987ce0
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-06-11 19:40:24 +0100

    tweaks to the delpoyment script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 #	Application
 #############################################################
 
-set :application, "bort"
-set :deploy_to, "/path/to/deploy"
+set :application, "MinimalPixel.net"
+set :deploy_to, "/home/admin/public_html/#{application}"
 
 #############################################################
 #	Settings
@@ -11,16 +11,19 @@ set :deploy_to, "/path/to/deploy"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+ssh_options[:keys] = %w(~/.ssh/saturn)
+ssh_options[:port] = 40000
 set :use_sudo, true
 set :scm_verbose, true
-set :rails_env, "production" 
+set :rails_env, "production"
+set :starling_port, 15151
 
 #############################################################
 #	Servers
 #############################################################
 
-set :user, "bort"
-set :domain, "www.example.com"
+set :user, "admin"
+set :domain, "www.minimalpixel.net"
 server domain, :app, :web
 role :db, domain, :primary => true
 
@@ -30,10 +33,10 @@ role :db, domain, :primary => true
 
 set :scm, :git
 set :branch, "master"
-set :scm_user, 'bort'
-set :scm_passphrase, "PASSWORD"
-set :repository, "git@github.com:FudgeStudios/bort.git"
-set :deploy_via, :remote_cache
+# set :scm_user, 'bort'
+# set :scm_passphrase, "PASSWORD"
+set :repository, "git@github.com:olivernn/MinimalPixel.git"
+set :deploy_via, :copy
 
 #############################################################
 #	Passenger
@@ -46,27 +49,25 @@ namespace :deploy do
     production:    
       adapter: mysql
       encoding: utf8
-      username: root
-      password: 
-      database: bort_production
+      username: minimalpixel
+      password: computer
+      database: minimalpixel_production
       host: localhost
     EOF
     
     put db_config, "#{release_path}/config/database.yml"
-    
+
     #########################################################
     # Uncomment the following to symlink an uploads directory.
     # Just change the paths to whatever you need.
     #########################################################
     
-    # desc "Symlink the upload directories"
-    # task :before_symlink do
-    #   run "mkdir -p #{shared_path}/uploads"
-    #   run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
-    # end
-  
+    desc "Create symlink to shared folder"
+    task :create_symlink do
+      run "ln -nfs #{shared_path}/sources #{release_path}/public/system/sources"
+    end
   end
-    
+  
   # Restart passenger on deploy
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
@@ -76,6 +77,41 @@ namespace :deploy do
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
+  end  
+end
+
+#############################################################
+#	Starling & Workling
+#############################################################
+
+namespace :workling do
+  desc "Restart Workling"
+  task :restart do
+    run "#{deploy_to}/current/script/workling_client stop"
+    run "RAILS_ENV=production #{deploy_to}/current/script/workling_client start"
   end
   
+  desc "Start Workling"
+  task :start do
+    run "RAILS_ENV=production #{deploy_to}/current/script/workling_client start"
+  end
+  
+  desc "Stop Workling"
+  task :stop do
+    run "#{deploy_to}/current/script/workling_client stop"
+  end
 end
+
+namespace :starling do
+  desc "Start Starling"
+  task :start do
+    run "starling -d -p #{starling_port}"
+  end
+end
+
+#############################################################
+#	callbacks
+#############################################################
+
+after :deploy, "deploy:cleanup"
+after :update_code, "workling:restart"
