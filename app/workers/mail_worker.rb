d71bf7f637749a4ab18c200b6d4b2e4017f029a2
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-07-06 15:27:06 +0100

    trying to fix the IE bugs, also applying a fix to the contact us action

--- app/workers/mail_worker.rb
+++ app/workers/mail_worker.rb
@@ -3,7 +3,7 @@ class MailWorker < Workling::Base
     Contact.deliver_contact_user(options[:contact_form])
   end
   
-  def contact_us
+  def contact_us(options)
     Contact.deliver_contact_us(options[:contact_form])
   end
 end
