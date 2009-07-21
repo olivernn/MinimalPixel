152c48cee3040a6edd92b9977befcb83c98e05e6
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-07-21 18:21:35 +0100

    made sure the style sweeper clears the whole cache

--- app/sweepers/style_sweeper.rb
+++ app/sweepers/style_sweeper.rb
@@ -18,11 +18,14 @@ class StyleSweeper < ActionController::Caching::Sweeper
   private
   
   def expire_style_cache(style)
-    expire_fragment :controller => :styles, :action => :index, :format => :js
-    expire_fragment :controller => :styles, :action => :index, :format => :css
+    # expire_fragment :controller => :styles, :action => :index, :format => :js
+    # expire_fragment :controller => :styles, :action => :index, :format => :css
     
     # need to also sweep the item js show action as the item title doesn't pick-up the new styles
     # expire_fragment(%r{/items/})
-    expire_fragment(%r{#{style.user.subdomain}/items/show/*})
+    # expire_fragment(%r{#{style.user.subdomain}/items/show/*})
+    
+    # expire all fragments
+    expire_fragment(%r{#{style.user.subdomain}/*})
   end
 end
