714bd2fcd66f9ad233a9b719d7551bfbb37af4c1
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-07-21 17:34:33 +0100

    got the ProcessorWorker to expire the cache for the items after the processing has been finished, this allows us to still cache the item show action

--- app/workers/processor_worker.rb
+++ app/workers/processor_worker.rb
@@ -4,6 +4,7 @@ class ProcessorWorker < Workling::Base
     @image.start_processing!
     if @image.source.reprocess!
       @image.end_processing!
+      clear_cache(@image)
     else
       @image.error!
     end
@@ -13,8 +14,19 @@ class ProcessorWorker < Workling::Base
     @video = Video.find(options[:video_id])
     if @video.start_processing!
       @video.end_processing!
+      clear_cache(@video)
     else
       @video.error!
     end
+  end
+  
+  private
+  
+  # this violates MVC a little bit but we need to expire the cached item show page which currently has the "processing"
+  # message displayed.  It will also expire the project show page incase the user navigates back to it and triggers the
+  # caching without the newly processed item.
+  def clear_cache(item)
+    ActionController::Base.new.expire_fragment(%r{#{item.project.user.subdomain}/items/show/#{item.to_param}*})
+    ActionController::Base.new.expire_fragment(%r{#{item.project.user.subdomain}/projects/show/#{item.project.to_param}*})
   end
 end
