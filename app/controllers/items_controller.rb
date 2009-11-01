b97d6bf5167df8cda5d0ecf15ae8cd385a924e55
Oliver Nightingale <Oliver@oliver-nightingales-macbook.local>
2009-11-01 17:36:01 +0000

    adding an ajax call to the the items show page to check the status, then reloads if item is ready

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,7 @@ class ItemsController < MainController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @item }
+      format.json { render :json => @item }
       format.js # show.js.erb
     end
   end
